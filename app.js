fa9f29bfd8dfb6bc3fe413b66524ef2d3cc15009
Iremide Olatunji <olatunjiremide@gmail.com>
2024-02-07 14:30:21 +0100

    Views Changes

--- app.js
+++ app.js
@@ -13,14 +13,13 @@ const PORT = process.env.PORT;
 
 // Connecting to mongoDB
 mongoDB.connectToMongoDB()
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })); 
 
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'Views'));
-app.use('/birthday', router)
-
-app.use(express.json()) 
-app.use(express.urlencoded({ extended: true })); 
+app.use('/index', router.userRouter)
 
 
 // home route
